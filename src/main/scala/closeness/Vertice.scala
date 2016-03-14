6cec308e9b7e7ab094c5606fabd829498c4ecb7d
mtakeshi <marcos.takeshi@gmail.com>
2016-03-14 04:01:21 -0300

    implementada ordenacao por farness/closeness e webservice

---
+++ src/main/scala/closeness/Vertice.scala
@@ -0,0 +1,7 @@
+package closeness
+
+class Vertice(var farness: Int, var node: String, var closeness: Double) {
+  var farn: Int = farness
+  var nome: String = node
+  var clos: Double = closeness
+}
